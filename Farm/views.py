42a4ee7327771984cb1d7438f5556139e1372710
Aadith <maadith2002@gmail.com>
2024-03-20 19:34:56 +0530

    Updated

--- Farm/views.py
+++ Farm/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
@@ -20,5 +22,23 @@ def contact(request):
 def products(request):
     return render(request,'products.html')
 
-def login(request):
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return render(request, 'home.html')
+
+        else:
+            error_message = "Invalid username or password."
+            return render(request, 'register.html', {'error_message': error_message})
+    
     return render(request,'login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('index.html')
